ddb9d66fe3e76ab7867c6e6bef53a1f977ccc979
John Young <jwyoung0613@gmail.com>
2021-08-24 15:08:39 -0600

    added buttons and questions in js file

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,3 +1,24 @@
 var startButton = document.getElementById("start");
 var timerStart = document.getElementById("timer"); // countdown
 var questionEl = document.getElementById("question"); // questionElement
+var trueButton = document.getElementById("true");  // true
+var falseButton = document.getElementById("false");  // false
+var finishedTest = document.getElementById("finishedTest");  // completedTest
+var currentQuestion = 0;
+var timeLeft = 60;
+var score = 0;
+var questions = [
+    { q: "Is ECMAScript another name for javaScript?", a: true },
+    { q: "Using the HTML DOM, javaScript can access and change all elements of an HTML document?", a: true },
+    { q: "In javaScipt the === operator ignores data being logged?", a: false },
+    { q: "localStorage can be used to store and retrieve user data using javaScript?", a: true },
+    { q: "To access button clicks we use onselect in javaScript", a: false },
+]
+
+startButton.addEventListener("click", startQuiz);
+trueButton.addEventListener("click", () => answerQuestion(true));
+falseButton.addEventListener("click", () => answerQuestion(false));
+
+function startQuiz() {
+    
+}
