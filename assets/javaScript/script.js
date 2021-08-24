b4b99e15fd7f799779504a11e2f1a9d613401e4b
John Young <jwyoung0613@gmail.com>
2021-08-24 14:47:04 -0600

    setup initial html and started js

---
+++ assets/javaScript/script.js
@@ -0,0 +1,3 @@
+var startButton = document.getElementById("start");
+var timerStart = document.getElementById("timer"); // countdown
+var questionEl = document.getElementById("question"); // questionElement
